7b7b202c1fa5348d58aba1a71b7b3adcfe6abff0
Michael O'Connell <mikematoconnell@gamil.com>
2026-03-22 13:13:41 -0400

    completed first tutorial

---
+++ forkprojectapplication/firstapp/titanic.py
@@ -0,0 +1,38 @@
+import os
+
+import pandasai as pai
+from pandasai_litellm.litellm import LiteLLM
+
+CLAUDE_API_KEY = os.getenv("CLAUDE_API_KEY")
+
+# from documentation not from tutorial as the package has been updated
+llm = LiteLLM(model="anthropic/claude-sonnet-4-20250514", api_key=CLAUDE_API_KEY)
+
+pai.config.set({"llm": llm})
+
+df = pai.read_csv("titanic_data.csv")
+
+df.head()
+
+response = df.chat("What sex was most likely to have survived?")
+
+print(response)
+
+males = df[df["Sex"] == "male"]
+
+males["Survived"].sum() / len(males)
+
+females = df[df["Sex"] == "female"]
+
+females["Survived"].sum() / len(females)
+
+response_1 = df.chat("What can you tell us about the class of the passenger - is there a correlation wiht survival rate?")
+
+print(response_1)
+
+response_2 = df.chat("Plot the histogram of survivorship of the sexes")
+
+print(response_2)
+
+response_3 = df.chat("Create a histogram plot based on the fare values")
+print(response_3)
